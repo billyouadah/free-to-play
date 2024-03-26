2e4d551b3a8768cfb4ec3d14e1ee2e57e9e39099
Billy <billyouadah@gmail.com>
2024-03-26 16:36:24 +0100

    hybride vs header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DisplayHeader from "./components/Header/DisplayHeader";
 import DisplayFooter from "./components/Header/DisplayFooter";
 import SearchBar from "./components/Header/SearchBar";
 import DisplayCatalogue from "./components/DisplayCatalogue";
-import DisplayGames from './components/DisplayGames'
-import './App.css'
+import DisplayGames from "./components/DisplayGames";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      <DisplayGames></DisplayGames>
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <DisplayHeader />
+        <SearchBar />
+        <Routes>
+          <Route path="/Catalogue" element={<DisplayCatalogue />} />
+          <Route path="/Home" element={<DisplayGames />} />
+        </Routes>
+        <DisplayFooter />
+      </BrowserRouter>
+    </div>
   );
 }
 
